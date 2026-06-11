a3206b22415cb34154d561f0ac03f334f15898a3
Daniela Pamely <dpev.arq@gmail.com>
2026-06-10 22:44:31 -0600

    feat: complete ingresar experience for museo

---
+++ app/arte/components/HeroIngresar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function HeroIngresar() {
+  return (
+    <div
+      className="
+        w-full
+        h-[10vh]
+      "
+    />
+  );
+}
--- app/arte/ingresar/page.tsx
+++ app/arte/ingresar/page.tsx
@@ -1,3 +1,91 @@
+"use client";
+
+import NavigationMuseo from "../components/NavigationMuseo";
+import HeroIngresar from "../components/HeroIngresar";
+import LoginCard from "../components/LoginCard";
+import FooterMuseo from "../components/FooterMuseo";
+
 export default function Page() {
-  return <main></main>;
+  return (
+    <>
+      <NavigationMuseo
+        onOpenConversation={() => {}}
+      />
+
+      <main
+  className="
+    relative
+    w-full
+    min-h-screen
+
+    flex
+    flex-col
+    items-center
+
+    overflow-hidden
+  "
+>
+
+{/* Background */}
+
+<div
+  className="absolute inset-0 -z-10"
+  style={{
+    backgroundImage: "url('/fondoingresar.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  }}
+/>
+
+{/* Overlay */}
+
+<div
+  className="absolute inset-0 -z-10"
+  style={{
+    background:
+      "linear-gradient(rgba(5,9,16,.18), rgba(5,9,16,.18))",
+  }}
+/>
+
+       <div
+  className="
+    relative
+
+    w-full
+    min-h-screen
+
+    flex
+    flex-col
+    items-center
+    justify-center
+  "
+>
+  <HeroIngresar />
+
+  <div
+  className="
+    absolute
+
+    left-1/2
+    -translate-x-1/2
+
+    top-[52%]
+    -translate-y-1/2
+
+    w-full
+
+    flex
+    justify-center
+
+    px-6
+  "
+>
+  <LoginCard />
+</div>
+</div>
+<FooterMuseo />
+      </main>
+    </>
+  );
 }
